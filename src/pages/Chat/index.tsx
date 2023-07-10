39a2a794f9240e569646d694e221842cd594c70e
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-07-09 20:45:34 -0600

    chore: add button to view

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -7,9 +7,19 @@ import Text from '@components/atoms/Text'
 const Chat: React.FC<RootStackScreenProps<'Chat'>> = () => {
   return (
     <Container>
-      <Button type="primary" text="Hello" />
       <Text type="pLarge" color="foreground0">
         Chat
+      </Text>
+      <Button
+        text="Press me!"
+        color="primary"
+        textType="buttonMedium"
+        size="block"
+        onPress={() => console.log('Pressed!')}
+        onFocus={() => console.log('Focused!')}
+      />
+      <Text type="h1" color="foreground0">
+        Hello
       </Text>
     </Container>
   )
