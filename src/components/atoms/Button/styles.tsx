5c1a9114d1671f4f97868753df7929a95e7df083
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-07-09 20:45:19 -0600

    FEAT: button component

--- src/components/atoms/Button/styles.tsx
+++ src/components/atoms/Button/styles.tsx
@@ -1,11 +1,39 @@
 import styled from '@emotion/native'
-import { ButtonProps } from '.'
+import { Colors } from '@interfaces/themes'
 
-export const ButtonWrapper = styled.TouchableOpacity<
-  Pick<ButtonProps, 'type' | 'styles' | 'width' | 'borderRadius'>
->(({ theme, type, borderRadius, width }) => ({
-  backgroundColor: theme.colors[type],
-  width: width || '100%',
-  borderRadius: borderRadius || 0,
-  padding: theme.sizes.md,
+export type ButtonSize = 'small' | 'medium' | 'large' | 'block'
+
+export const ButtonContainer = styled.View(() => ({
+  alignItems: 'flex-start',
 }))
+
+export const ButtonWrapper = styled.TouchableOpacity<{
+  color: keyof Colors | undefined
+  borderRadius: number | undefined
+  size: ButtonSize
+}>(({ theme, color, borderRadius, size }) => {
+  const paddingVerticalMapping: Record<ButtonSize, number> = {
+    small: theme.sizes.xxs,
+    medium: theme.sizes.xs,
+    large: theme.sizes.sm,
+    block: theme.sizes.sm,
+  }
+  const paddingHorizontalMapping: Record<ButtonSize, number> = {
+    small: theme.sizes.sm,
+    medium: theme.sizes.lg,
+    large: theme.sizes.xxl,
+    block: theme.sizes.xxl,
+  }
+  return {
+    paddingVertical: paddingVerticalMapping[size],
+    paddingHorizontal: paddingHorizontalMapping[size],
+    width: size === 'block' ? '100%' : undefined,
+    maxWidth: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: borderRadius ? borderRadius : 0,
+    alignSelf: 'flex-start',
+    flexGrow: 0,
+    backgroundColor: color ? theme.colors[color] : theme.colors.primary,
+  }
+})
