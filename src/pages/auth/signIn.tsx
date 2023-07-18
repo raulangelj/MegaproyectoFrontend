d39bc092120709ac5e57d09fe12d8a1cadac71f3
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-07-18 00:08:26 -0600

    feat: sign in component

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -1,21 +1,51 @@
 import React from 'react'
-import { EmailPaswordContainer, SignInScreen } from './styles'
+import {
+  EmailPaswordContainer,
+  ForgotPasswordButton,
+  LoginSmallButtonsContainer,
+  SignInScreen,
+  SignInButton,
+  ForgotPasswordText,
+  InputField,
+  CreateAccountContainer,
+  CreateAccountBtn,
+} from './styles'
 import Text from '@components/atoms/Text'
-import Input from '@components/molecules/Input'
-
+import { Checkbox } from 'native-base'
+import { OrStray } from '@components/atoms/OrStray'
+import SocialBtns from '@components/atoms/SocialBtns'
 export const SignIn = () => {
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
 
-  const InputStyles = {
-    marginBottom: 20,
+  const onForgotPassword = () => {
+    console.log('Forgot Password')
+  }
+
+  const onSignIn = () => {
+    console.log('Sign In')
+  }
+
+  const onFacebookPress = () => {
+    console.log('Facebook')
+  }
+
+  const onGooglePress = () => {
+    console.log('Google')
+  }
+
+  const onTwitterPress = () => {
+    console.log('Twitter')
+  }
+  const onCreateAccount = () => {
+    console.log('Create Account')
   }
 
   return (
     <SignInScreen>
       <Text type="h1">Sign In</Text>
       <EmailPaswordContainer>
-        <Input
+        <InputField
           showLabel
           label="Email Address"
           icon="email-variant"
@@ -23,9 +53,8 @@ export const SignIn = () => {
           onChangeText={setEmail}
           placeholder="email@gmail.com"
           keyboardType="email-address"
-          style={InputStyles}
         />
-        <Input
+        <InputField
           showLabel
           label="Password"
           icon="lock-open"
@@ -33,9 +62,39 @@ export const SignIn = () => {
           onChangeText={setPassword}
           placeholder="* * * * * *"
           isPassword
-          style={InputStyles}
+        />
+        <LoginSmallButtonsContainer>
+          <Checkbox value="rememberMe">
+            <Text type="pSmall">Remember me</Text>
+          </Checkbox>
+          <ForgotPasswordButton onPress={onForgotPassword}>
+            <ForgotPasswordText type="pSmall">
+              Forgot Password?
+            </ForgotPasswordText>
+          </ForgotPasswordButton>
+        </LoginSmallButtonsContainer>
+        <SignInButton
+          textType="buttonMedium"
+          size="block"
+          onPress={onSignIn}
+          text="Sign In 2"
+          borderRadius={50}
         />
       </EmailPaswordContainer>
+      <OrStray />
+      <SocialBtns
+        onFacebookPress={onFacebookPress}
+        onGooglePress={onGooglePress}
+        onTwitterPress={onTwitterPress}
+      />
+      <CreateAccountContainer>
+        <Text type="pSmall">Don't have an account?</Text>
+        <CreateAccountBtn onPress={onCreateAccount}>
+          <Text type="pSmall" color="quinary">
+            Create Account
+          </Text>
+        </CreateAccountBtn>
+      </CreateAccountContainer>
     </SignInScreen>
   )
 }
