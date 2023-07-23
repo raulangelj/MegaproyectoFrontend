6347b00238c2c88dc92b38729f5d2db5afd3f3e4
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-07-23 12:01:23 -0600

    feat: sign up
    Add the sign up screen
    Remove the check box

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -9,9 +9,9 @@ import {
   InputField,
   CreateAccountContainer,
   CreateAccountBtn,
+  ScrollView,
 } from './styles'
 import Text from '@components/atoms/Text'
-import { Checkbox } from 'native-base'
 import { OrStray } from '@components/atoms/OrStray'
 import SocialBtns from '@components/atoms/SocialBtns'
 import { RootStackScreenProps } from '@navigations/types/ScreenProps'
@@ -46,57 +46,59 @@ export const SignIn: React.FC<RootStackScreenProps<'SignIn'>> = ({
   }
 
   return (
-    <SignInScreen>
-      <Text type="h1">Sign In</Text>
-      <EmailPaswordContainer>
-        <InputField
-          label="Email Address"
-          icon="email-variant"
-          value={email}
-          onChangeText={setEmail}
-          placeholder="email@gmail.com"
-          keyboardType="email-address"
+    <ScrollView>
+      <SignInScreen>
+        <Text type="h1">Sign In</Text>
+        <EmailPaswordContainer>
+          <InputField
+            label="Email Address"
+            icon="email-variant"
+            value={email}
+            onChangeText={setEmail}
+            placeholder="email@gmail.com"
+            keyboardType="email-address"
+          />
+          <InputField
+            label="Password"
+            icon="lock-open"
+            value={password}
+            onChangeText={setPassword}
+            placeholder="* * * * * *"
+            isPassword
+          />
+          <LoginSmallButtonsContainer>
+            {/* <Checkbox value="rememberMe">
+              <Text type="pSmall">Remember me</Text>
+            </Checkbox> */}
+            <ForgotPasswordButton onPress={onForgotPassword}>
+              <ForgotPasswordText type="pSmall">
+                Forgot Password?
+              </ForgotPasswordText>
+            </ForgotPasswordButton>
+          </LoginSmallButtonsContainer>
+          <SignInButton
+            textType="buttonMedium"
+            size="block"
+            onPress={onSignIn}
+            text="Sign In"
+            borderRadius={50}
+          />
+        </EmailPaswordContainer>
+        <OrStray />
+        <SocialBtns
+          onFacebookPress={onFacebookPress}
+          onGooglePress={onGooglePress}
+          onTwitterPress={onTwitterPress}
         />
-        <InputField
-          label="Password"
-          icon="lock-open"
-          value={password}
-          onChangeText={setPassword}
-          placeholder="* * * * * *"
-          isPassword
-        />
-        <LoginSmallButtonsContainer>
-          <Checkbox value="rememberMe">
-            <Text type="pSmall">Remember me</Text>
-          </Checkbox>
-          <ForgotPasswordButton onPress={onForgotPassword}>
-            <ForgotPasswordText type="pSmall">
-              Forgot Password?
-            </ForgotPasswordText>
-          </ForgotPasswordButton>
-        </LoginSmallButtonsContainer>
-        <SignInButton
-          textType="buttonMedium"
-          size="block"
-          onPress={onSignIn}
-          text="Sign In"
-          borderRadius={50}
-        />
-      </EmailPaswordContainer>
-      <OrStray />
-      <SocialBtns
-        onFacebookPress={onFacebookPress}
-        onGooglePress={onGooglePress}
-        onTwitterPress={onTwitterPress}
-      />
-      <CreateAccountContainer>
-        <Text type="pSmall">Don't have an account?</Text>
-        <CreateAccountBtn onPress={onCreateAccount}>
-          <Text type="pSmall" color="quinary">
-            Create Account
-          </Text>
-        </CreateAccountBtn>
-      </CreateAccountContainer>
-    </SignInScreen>
+        <CreateAccountContainer>
+          <Text type="pSmall">Don't have an account?</Text>
+          <CreateAccountBtn onPress={onCreateAccount}>
+            <Text type="pSmall" color="quinary">
+              Create Account
+            </Text>
+          </CreateAccountBtn>
+        </CreateAccountContainer>
+      </SignInScreen>
+    </ScrollView>
   )
 }
